1ffbb9525360bc8f7502252bb38c881f1c0b77fc
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-08-27 23:26:36 +1200

    FE: Imported Noto Sans font, added the exercise placeholders with the relevant text below.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <head>
+                <title>Poker Trainer</title>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100&display=swap" rel="stylesheet"/>
+            </head>
+                <header className="App-header">
+                    <div className="exercise">
+                        <div className="exShape" id="ex1"> </div>
+                        <div className="exShape" id="ex2"> </div>
+                        <div className="exShape" id="ex3"> </div>
+                        <div className="exShape" id="ex4"> </div>
+                    <div className="exerciseText" id="ex1text">Exercise 1</div>
+                    <div className="exerciseText" id="ex2text">Exercise 2</div>
+                    <div className="exerciseText" id="ex3text">Exercise 3</div>
+                    <div className="exerciseText" id="ex4text">Exercise 4</div>
+                    </div>
+                </header>
+            <body>
+
+            </body>
+        </div>
+);
 }
 
 export default App;
