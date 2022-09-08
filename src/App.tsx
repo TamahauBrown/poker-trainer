8e34dc73e7ea2a3e3e5bfa3e065afbc9c338ef6d
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-08 19:54:41 +1200

    Added playing cards and poker table

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,25 @@ function App() {
                 <title>Poker Trainer</title>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100&display=swap" rel="stylesheet"/>
             </head>
-                <header className="App-header">
-                    <div className="exercise">
-                        <div className="exShape" id="ex1"> </div>
-                        <div className="exShape" id="ex2"> </div>
-                        <div className="exShape" id="ex3"> </div>
-                        <div className="exShape" id="ex4"> </div>
+            <header className="App-header">
+                <div className="exercise">
+                    <div className="exShape" id="ex1"> </div>
+                    <div className="exShape" id="ex2"> </div>
+                    <div className="exShape" id="ex3"> </div>
+                    <div className="exShape" id="ex4"> </div>
                     <div className="exerciseText" id="ex1text">Exercise 1</div>
                     <div className="exerciseText" id="ex2text">Exercise 2</div>
                     <div className="exerciseText" id="ex3text">Exercise 3</div>
                     <div className="exerciseText" id="ex4text">Exercise 4</div>
-                    </div>
-                </header>
-            <body>
-            <div className="sidebar"> </div>
-            </body>
+                </div>
+                <div className="sidebar"> </div>
+                <div className="PokerTable">
+                    <h1 className="TableText"> Poker Trainer </h1>
+                </div>
+
+            </header>
         </div>
-);
+    );
 }
 
 export default App;
