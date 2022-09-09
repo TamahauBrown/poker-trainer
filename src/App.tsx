2f0d4c6e3ebb4b9af67e741b25503b0b4daa8fc3
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-10 09:37:03 +1200

    FE table is in position for the look, needs to be fixed to work with multiple screen sizes, plan of attack is grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -20,12 +21,39 @@ function App() {
                 </div>
                 <div className="sidebar"> </div>
                 <div className="floor">
-                <div className="PokerTable">
-                    <div id="playerCards">
-                    <img src={"./Images/backs/red.svg"} alt="Player card left" className="playerCards" id="leftCard" />
-                    <img src={"./Images/backs/red.svg"} alt="Player card right" className="playerCards" id="rightCard" />
+                    <div className="pokerTable">
+                        <div id="playerCards">
+                            <img src={"./Images/backs/red.svg"} alt="Player card left" className="playerCards"
+                                 id="leftCard"/>
+                            <img src={"./Images/backs/red.svg"} alt="Player card right" className="playerCards"
+                                 id="rightCard"/>
+                        </div>
+                        <div id="tableCards">
+                            <img src={"./Images/backs/red.svg"} alt="Flop 1" className="tableCards" id="flop1"/>
+                            <img src={"./Images/backs/red.svg"} alt="Flop 2" className="tableCards" id="flop2"/>
+                            <img src={"./Images/backs/red.svg"} alt="Flop 3" className="tableCards" id="flop3"/>
+                            <img src={"./Images/backs/red.svg"} alt="Turn" className="tableCards" id="turn"/>
+                            <img src={"./Images/backs/red.svg"} alt="River" className="tableCards" id="river"/>
+                        </div>
+                        <div id="opponentCards">
+                            <div id="opponentHand1">
+                                <img src={"./Images/backs/red.svg"} alt="Hand1Left" className="hand1" id=""/>
+                                <img src={"./Images/backs/red.svg"} alt="Hand1Right" className="hand1" id=""/>
+                            </div>
+                            <div id="opponentHand2">
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand2Left" className="hand2" id=""/>
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand2Right" className="hand2" id=""/>
+                            </div>
+                            <div id="opponentHand3">
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand3Right" className="hand3" id=""/>
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand4Left" className="hand4" id=""/>
+                            </div>
+                            <div id="opponentHand4">
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand3Right" className="hand4" id=""/>
+                                <img src={"./Images/backs/red.svg"} alt="OpponentHand5Left" className="hand5" id=""/>
+                            </div>
+                        </div>
                     </div>
-                </div>
                 </div>
 
             </header>
