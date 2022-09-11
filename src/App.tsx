efeef3c8d29562ead3db78125dae1e052f8e041a
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-11 14:41:25 +1200

    Fixing card size and locations for fixed resolution

--- src/App.tsx
+++ src/App.tsx
@@ -65,15 +65,20 @@ function App() {
                 </div>
                 <div className="floor">
                     <div className="pokerTable">
+                        <div id="playerCards">
                         <img src={"./Images/backs/red.svg"} alt="Player card left" className="playerCards"
                              id="leftCard"/>
                         <img src={"./Images/backs/red.svg"} alt="Player card right" className="playerCards"
                              id="rightCard"/>
+                        </div>
+                        <div id="tableCards">
                         <img src={"./Images/backs/red.svg"} alt="Flop 1" className="tableCards" id="flop1"/>
                         <img src={"./Images/backs/red.svg"} alt="Flop 2" className="tableCards" id="flop2"/>
                         <img src={"./Images/backs/red.svg"} alt="Flop 3" className="tableCards" id="flop3"/>
                         <img src={"./Images/backs/red.svg"} alt="Turn" className="tableCards" id="turn"/>
                         <img src={"./Images/backs/red.svg"} alt="River" className="tableCards" id="river"/>
+                        </div>
+                        <div id="oppCards">
                         <img src={"./Images/backs/red.svg"} alt="Hand1Left" className="hand1" id="oh1l"/>
                         <img src={"./Images/backs/red.svg"} alt="Hand1Right" className="hand1" id="ohr1"/>
                         <img src={"./Images/backs/red.svg"} alt="OpponentHand2Left" className="hand2" id="ohl2"/>
@@ -84,6 +89,7 @@ function App() {
                         <img src={"./Images/backs/red.svg"} alt="OpponentHand5Left" className="hand5" id="ohr4"/>
                         <img src={"./Images/backs/red.svg"} alt="OpponentHand3Right" className="hand4" id="ohl5"/>
                         <img src={"./Images/backs/red.svg"} alt="OpponentHand5Left" className="hand5" id="ohr5"/>
+                        </div>
                     </div>
                     <Box sx={{width: 500}} className="slider">
                         <Typography id="input-slider" gutterBottom>
