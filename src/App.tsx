0a217dc7bcfd1f4434865a85b53a9f81862cb224
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-13 06:30:09 +1200

    FE is not working "TS2322: Type '{ value: number; onChange: (event: Event, newValue: number | number[]) => void; "aria-labelledby": string; }' is not assignable to type 'IntrinsicAttributes'.   Property 'value' does not exist on type 'IntrinsicAttributes'." on line 46 in Slider.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 //import React from 'react';
 import './App.scss';
 import * as React from 'react';
-import {styled} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Slider from '@mui/material/Slider';
-import MuiInput from '@mui/material/Input';
-import SendIcon from '@mui/icons-material/Send';
-import Button from '@mui/material/Button';
 
 // @ts-ignore
 import {PokerTable} from "./Components/PokerTable.tsx";
@@ -17,32 +9,10 @@ import {ExerciseHeader} from "./Components/ExerciseHeader.tsx";
 // @ts-ignore
 import {Sidebar} from "./Components/Sidebar.tsx"
 
-
-const Input = styled(MuiInput)`
-  width: 42px;
-`;
+// @ts-ignore
+import {Slider} from "./Components/Slider.tsx"
 
 function App() {
-    const [value, setValue] = React.useState<number | string | Array<number | string>>(
-        30,
-    );
-
-    const handleSliderChange = (event: Event, newValue: number | number[]) => {
-        setValue(newValue);
-    };
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value === '' ? '' : Number(event.target.value));
-    };
-
-    const handleBlur = () => {
-        if (value < 0) {
-            setValue(0);
-        } else if (value > 100) {
-            setValue(100);
-        }
-    };
-
     return (
         <div className="App">
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100&display=swap" rel="stylesheet"/>
@@ -51,46 +21,7 @@ function App() {
                <ExerciseHeader> </ExerciseHeader>
                 <div id="floor">
                     <PokerTable> </PokerTable>
-
-                    <div id="sliderFunctions">
-                        <Box sx={{width: 500}} className="slider">
-                            <Typography id="input-slider" gutterBottom>
-                                <b id="chances">What are your chances of winning?</b>
-                            </Typography>
-                            <Grid container spacing={2} alignItems="center">
-                                <Grid item>
-                                </Grid>
-                                <Grid item xs>
-                                    <Slider
-                                        value={typeof value === 'number' ? value : 0}
-                                        onChange={handleSliderChange}
-                                        aria-labelledby="input-slider"
-                                    />
-                                </Grid>
-                                <Grid item>
-                                    <Input
-                                        id="chanceBox"
-                                        value={value}
-                                        size="small"
-                                        onChange={handleInputChange}
-                                        onBlur={handleBlur}
-                                        inputProps={{
-                                            step: 10,
-                                            min: 0,
-                                            max: 100,
-                                            type: 'number',
-                                            'aria-labelledby': 'input-slider',
-                                        }}
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Box>
-                        <div id="submitGuess">
-                            <Button variant="contained" endIcon={<SendIcon/>}>
-                                SUBMIT
-                            </Button>
-                        </div>
-                    </div>
+                    <Slider> </Slider>
                 </div>
             </header>
         </div>
--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,7 +1,76 @@
 // import VolumeUp from '@mui/icons-material/VolumeUp';
 import {styled} from "@mui/material/styles";
 import MuiInput from "@mui/material/Input";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import * as React from "react"
 
 const Input = styled(MuiInput)`
   width: 42px;
 `;
+
+export function Slider() {
+    const [value, setValue] = React.useState<number | string | Array<number | string>>(
+        30,
+    );
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue);
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value === '' ? '' : Number(event.target.value));
+    };
+
+    const handleBlur = () => {
+        if (value < 0) {
+            setValue(0);
+        } else if (value > 100) {
+            setValue(100);
+        }
+    };
+    return(
+        <div id="sliderFunctions">
+            <Box sx={{width: 500}} className="slider">
+                <Typography id="input-slider" gutterBottom>
+                    <b id="chances">What are your chances of winning?</b>
+                </Typography>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider
+                            value={typeof value === 'number' ? value : 0}
+                            onChange={handleSliderChange}
+                            aria-labelledby="input-slider"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Input
+                            id="chanceBox"
+                            value={value}
+                            size="small"
+                            onChange={handleInputChange}
+                            onBlur={handleBlur}
+                            inputProps={{
+                                step: 10,
+                                min: 0,
+                                max: 100,
+                                type: 'number',
+                                'aria-labelledby': 'input-slider',
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+            <div id="submitGuess">
+                <Button variant="contained" endIcon={<SendIcon/>}>
+                    SUBMIT
+                </Button>
+            </div>
+        </div>
+    )
+}
