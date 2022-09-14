933a95ac11e986260377817ca737aa2f29fcb4d8
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-14 22:01:18 +1200

    Player hands functionality in equity estimate

--- src/Components/ExerciseHeader.tsx
+++ src/Components/ExerciseHeader.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import {invoke} from "@tauri-apps/api/tauri";
 
 export function ExerciseHeader() {
-    const board: String[] = [];
-    const opp_hands: String[] = [];
-    const player_hands: String[] = [];
     var full_path;
     const image_path = "./Images/fronts/";
     const svg = ".svg";
@@ -18,25 +15,27 @@ export function ExerciseHeader() {
 
     function player_cards(entries){
         //Player hands
-        const left = entries[2][1][0][0];
-        const right = entries[2][1][0][1];
+        console.log(entries[3]);
+        const left = entries[3][1][0];
+        const right = entries[3][1][1];
 
         //Left player hand
         entries = Object.entries(left);
-        full_path = image_path + entries[0][1].toString().toLowerCase() + "s_" + entries[1][1].toString().toLowerCase() + svg;
-        console.log(full_path);
+        full_path = image_path + entries[0][1].toString().toLowerCase() + "_" + entries[1][1].toString().toLowerCase() + svg;
         const leftCard = document.getElementById("leftCard") as HTMLImageElement;
         leftCard.src = full_path;
         //Right player hand
         entries = Object.entries(right);
-        player_hands.push(full_path);
-        //console.log(player_hands);
+        full_path = image_path + entries[0][1].toString().toLowerCase() + "_" + entries[1][1].toString().toLowerCase() + svg;
+        const rightCard = document.getElementById("rightCard") as HTMLImageElement;
+        rightCard.src = full_path;
     }
 
     function equity_estimate() {
         let result = invoke('equity_estimate', {});
         result.then(res => {
             hands(res);
+            console.log(res);
             //console.log(board);
             //console.log(opp_hands);
             //console.log(player_hands);
