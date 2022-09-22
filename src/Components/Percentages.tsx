a5a77414fef9a86b803df869644549726fe7bbbc
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-09-22 19:58:37 +1200

    FE refactor of poker table into multiple components and GUI update to make player hand easier to find

---
+++ src/Components/Percentages.tsx
@@ -0,0 +1,11 @@
+export function Percentages() {
+    return(
+                  <div id="percentages">
+                      <h1 id="percentage1" className="calculations"> 0% </h1>
+                      <h1 id="percentage2" className="calculations"> 0% </h1>
+                      <h1 id="percentage3" className="calculations"> 0% </h1>
+                      <h1 id="percentage4" className="calculations"> 0% </h1>
+                      <h1 id="percentage5" className="calculations"> 0% </h1>
+                  </div>
+    );
+}
