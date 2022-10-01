a90a7f0b2ee44dd27eedc3c52005fd16646ff5a5
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-10-02 08:53:16 +1300

    GUI update. Removed original black sidebar, changed opacity on hamburger menu extended and fixed card positioning

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
     return (
         <div className="App">
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100&display=swap" rel="stylesheet"/>
-            <Sidebar />
             <header className="App-header">
                 <ExerciseHeader />
                 <PokerTable />
